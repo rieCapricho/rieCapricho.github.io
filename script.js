a5a54ade07abf454dfad2436170580f4d614ba37
Abrienne <ajcapricho@mcm.edu.ph>
2025-02-03 09:17:50 +0800

    new

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 document.getElementById("enterBtn").addEventListener("click", function() {
-    document.getElementById("landing-page").classList.add("hidden");
+    document.querySelector(".landing").classList.add("hidden");
     document.getElementById("home-page").classList.remove("hidden");
-});
-
-// Sidebar navigation effect
-document.querySelectorAll(".sidebar li").forEach(item => {
-    item.addEventListener("click", () => {
-        document.querySelectorAll(".sidebar li").forEach(li => li.classList.remove("active"));
-        item.classList.add("active");
-    });
 });
